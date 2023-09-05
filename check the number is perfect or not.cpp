2670a388e0cd163c48f1556db87b1821cea9bf8f
CsTechAmit <127652046+CsTechAmit@users.noreply.github.com>
2023-09-05 23:40:35 +0530

    Add files via upload
    check the number is perfect nor not

---
+++ check the number is perfect or not.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int n,i,sum=0;
+	printf("enter the number :");
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{	
+		sum=sum+i;
+	}
+	}
+	if (sum==n)
+	printf("enter the number is perfect");
+	else
+	printf("the is not perfact number");
+}
